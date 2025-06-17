c6e10873ce4bb82d1f9b30cf9ea1297700bc9fa3
Vignesh D <vignesh.d9123@gmail.com>
2025-06-17 22:30:57 +0530

    Change service config for custom backend

--- express-backend/src/index.ts
+++ express-backend/src/index.ts
@@ -4,9 +4,10 @@ const app = express()
 
 
 app.get("/pid", (req: Request, res: Response)=>{
+	console.log("Request came")
 	res.json({pid:process.pid})
 })
 
 app.listen(3000, ()=>{
-	console.log("Server running on port 3000")	
+	console.log("Server running on port 3000", process.pid)	
 })
